c6a0a58e19dece22f8c38b034d57cfa1fde5a972
Ann <ann.millspaugh@gmail.com>
2016-11-11 12:53:11 -0800

    add styling to homepage and summary

--- jinja_complier.py
+++ jinja_complier.py
@@ -1,6 +1,13 @@
 import os
 from jinja2 import Environment, FileSystemLoader
-from scraper import SYRACUSE_SITE_CATEGORIES
+from scraper import SYRACUSE_SITE_CATEGORIES, Page
+import pickle
+
+path = '.'
+all_pages = pickle.load(open(path+'/all_pages.pickle', 'rb'))
+broken_links = pickle.load(open(path+'/all_broken_links.pickle', 'rb'))
+top_pages = pickle.load(open(path+'/top_pages.pickle', 'rb'))
+
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -13,14 +20,18 @@ def render_template(template_filename, context):
 
 def create_index_html():
     fname = "index.html"
+
     svgs = []
     for category in SYRACUSE_SITE_CATEGORIES:
         name = category.replace('_',' ')
-        dpt = {'href':'graphs/' + category + '.html', 'name':name}
+        dpt = {'href':'graphs/' + category + '.html',
+                'name':name}
         svgs.append(dpt)
 
     context = {
-        'svgs': svgs
+        'svgs': svgs,
+        'all_pages_count':len(all_pages),
+        'broken_links_count':len(list(broken_links.elements()))
     }
 
     #
